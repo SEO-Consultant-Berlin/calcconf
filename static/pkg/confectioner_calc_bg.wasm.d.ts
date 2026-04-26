6c90ac272f1d3bbd985c1aee15afef2accd27e24
ТвоёИмя <@email.com>
2026-04-26 12:03:37 +0200

    Remove gitignore from pkg

---
+++ static/pkg/confectioner_calc_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const wasm_bindgen__convert__closures_____invoke__he95dfb84f6152fd4: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures________invoke__hbd0371d3cc46e04f: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures________invoke__h3eb6a2b27322e4ad: (a: number, b: number, c: any) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
